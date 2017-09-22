79776cc034e531d396497ca9303c2d99818458da
monarizasan <32188449+monarizasan@users.noreply.github.com>
2017-09-23 01:26:11 +0900

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -8,7 +8,7 @@ stty echo
 cat ./address.txt | while read line
 
 do
-ssh -t -t $line <<EOF
+ssh -t -t $line <<'EOF'
     cd chainpoint-node
     stty -echo
     echo "$password" | sudo -S make down; sudo git pull; sudo make up
