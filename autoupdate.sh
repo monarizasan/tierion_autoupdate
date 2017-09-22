f76b1ccbba329a6b5d4ef9f53d3a6126365f1e61
monarizasan <32188449+monarizasan@users.noreply.github.com>
2017-09-22 15:01:11 +0900

    Add files via upload

---
+++ autoupdate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+stty -echo
+printf "password: "
+read password
+stty echo
+
+cat ./ssh_ip.txt | while read line
+
+do
+ssh -t -t $line <<EOF
+    cd chainpoint-node
+    stty -echo
+    echo "$password" | sudo -S make down; sudo git pull; sudo make up
+    stty echo
+    exit
+EOF
+done;
